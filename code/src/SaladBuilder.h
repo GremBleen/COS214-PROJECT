7b82172432a05d99cce4c4cdd01bff79f0cd10f8
GremBleen <graemeharryblain@gmail.com>
2023-11-06 18:04:01 +0200

    Fixed issue in saladbuilder

--- code/src/SaladBuilder.h
+++ code/src/SaladBuilder.h
@@ -1,11 +1,11 @@
 /**
  * @file SaladBuilder.h
  * @brief Contains declaration for the SaladBuilder class
- * 
+ *
  * SaladBuilder is a concrete builder for the Salad class and inherits from SideBuilder.
  * It is responsible for building a Salad object using the template method pattern.
- * 
- * @authors 
+ *
+ * @authors
  * - Aidan Chapman (u22738917)
  * - Graeme Blain (u22625462)
 */
@@ -16,28 +16,27 @@
 #ifndef SALADBUILDER_H
 #define SALADBUILDER_H
 
-class SaladBuilder : public SideBuilder
-{
-    public:
-        SaladBuilder();
-        ~SaladBuilder();
+class SaladBuilder : public SideBuilder {
+public:
+    SaladBuilder();
+    ~SaladBuilder();
 
-        void washVegetables();
-        void chopVegetables();
-        void assembleSide();
-        void plateSide();
+    void washVegetables();
+    void chopVegetables();
+    void assembleSide();
+    void plateSide();
 
-        void washLettuce();
-        void cutLettuce();
-        void washTomato();
-        void cutTomato();
-        void washCucumber();
-        void cutCucumber();
-        void cutFeta();
-        void assembleSalad();
+    void washLettuce();
+    void cutLettuce();
+    void washTomato();
+    void cutTomato();
+    void washCucumber();
+    void cutCucumber();
+    void cutFeta();
+    void assembleSalad();
 
-    private:
-        Salad* salad; ///< Salad object
+private:
+    Salad* salad; ///< Salad object
 };
 
 #endif
